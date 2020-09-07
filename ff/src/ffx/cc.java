aaf5f0206cb73cc0b5643e7dd7e2955791b38da3
lzy2357657374 <2357657374@qq.com>
2020-09-07 16:48:11 +0800

    埃索达法杖

---
+++ ff/src/ffx/cc.java
@@ -0,0 +1,19 @@
+package ffx;
+
+public class cc {
+	public static void main(String[] args) {
+		String ta="a";
+		ta=ta.concat("b");
+	
+		String tb="c";
+		ta=ta.concat(tb);
+		System.out.println(ta);
+		
+				ta.replace('c', 'd');
+				System.out.println(ta);	
+				ta=ta.concat(tb);
+				System.out.println(ta);
+			
+	}
+
+}
